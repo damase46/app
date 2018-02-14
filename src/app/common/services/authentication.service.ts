1ec2c93a3ccfab92dd887cb65d9acf5f08d7e1cf
sergio <sergio_nod@hotmail.com>
2018-02-14 16:02:44 -0300

    prueba

---
+++ src/app/common/services/authentication.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {SessionStorageService} from 'ngx-webstorage';
+
+@Injectable()
+export class AuthenticationService {
+
+  apiAuthBaseUrl = 'http://projects-api.webtraining.zone';
+  user;
+  hasSession = false;
+
+  constructor(public http: HttpClient, public  locker: SessionStorageService) {
+
+  }
+
+  public isLoggedIn() {
+    const user = this.locker.retrieve('user');
+    if (!!user) {
+      this.user = user;
+      this.hasSession = true;
+    }
+    return this.hasSession;
+  }
+
+  public logIn(username: string, password: string) {
+    const url = `${this.apiAuthBaseUrl}/users/login`;
+    return this.http.post(url, {username: username, password: password});
+  }
+
+  public logout() {
+    this.user = null;
+    this.hasSession = false;
+    this.locker.clear('user');
+  }
+}
---
+++ src/app/public/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {AuthenticationService} from '../../common/services/authentication.service';
+import {SessionStorageService} from 'ngx-webstorage';
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
+  constructor( public authService: AuthenticationService, public locker: SessionStorageService, public router: Router) { }
+
+  getErrorMessageForUser() {
+    const hasError = this.username.hasError('required');
+    return hasError ? 'Nombre de usuario Requerido' : '';
+  }
+
+  getErrorMessageForPassword() {
+    const hasError = this.password.hasError('required');
+    return hasError ? 'Contraseña es requerida' : '';
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this.authService.logIn(this.username.value, this.password.value)
+      .subscribe((data) => {
+        this.authService.user = data;
+        this.locker.store('user', data);
+        this.router.navigate(['/home']);
+      }, (error: HttpErrorResponse) => {
+        if (error.status === 406) {
+          console.error('unable to login');
+        }
+        console.error(error);
+        this.authService.hasSession = false;
+      });
+  }
+}
